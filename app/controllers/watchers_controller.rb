90c206a1b376949ebe07e3183e7bd3f9a9e553c6
Paul Stengel <paul@wwwh.com>
2014-09-05 16:38:45 -0400

    Refactor #users_for_new_watcher_with_groups

--- app/controllers/watchers_controller.rb
+++ app/controllers/watchers_controller.rb
@@ -2,9 +2,13 @@ require_dependency "watchers_controller"
 
 class WatchersController < ApplicationController
   def users_for_new_watcher_with_groups
-    users = users_for_new_watcher_without_groups
-    users.concat(Group.all)
-    users -= Group.where(id: @watched.watcher_users) if @watched
+    users_for_new_watcher_without_groups.tap do |users|
+      if @watched
+        users.concat(Group.all - Group.where(id: @watched.watcher_users))
+      else
+        users.concat(Group.all)
+      end
+    end
   end
   alias_method_chain :users_for_new_watcher, :groups
 end
