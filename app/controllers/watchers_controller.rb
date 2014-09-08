a7de76be8f217819b403fd277c5971fd04bb818b
Paul Stengel <paul@wwwh.com>
2014-09-08 11:06:13 -0400

    Patch WatchersController#append

--- app/controllers/watchers_controller.rb
+++ app/controllers/watchers_controller.rb
@@ -1,6 +1,16 @@
 require_dependency "watchers_controller"
 
 class WatchersController < ApplicationController
+  def append_with_groups
+    append_without_groups
+
+    user_ids = params[:watcher][:user_ids] || params[:watcher][:user_id]
+    @users += Group.where(id: user_ids).all
+
+    render(nothing: true) if @users.blank?
+  end
+  alias_method_chain :append, :groups
+
   def users_for_new_watcher_with_groups
     users_for_new_watcher_without_groups.tap do |users|
       if @watched
