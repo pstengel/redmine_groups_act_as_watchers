21493bc3f375325f9b2de76229de6cea9c15a788
Paul Stengel <paul@wwwh.com>
2014-09-08 11:36:31 -0400

    Patch WatchersController#destroy

--- app/controllers/watchers_controller.rb
+++ app/controllers/watchers_controller.rb
@@ -9,6 +9,18 @@ class WatchersController < ApplicationController
   end
   alias_method_chain :append, :groups
 
+  def destroy_with_groups
+    @watched.set_watcher(User.where(id: params[:user_id]), false)
+    @watched.set_watcher(Group.where(id: params[:user_id]), false)
+
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+      format.api { render_api_ok }
+    end
+  end
+  alias_method_chain :destroy, :groups
+
   def users_for_new_watcher_with_groups
     users_for_new_watcher_without_groups.tap do |users|
       if @watched
